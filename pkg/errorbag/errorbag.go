a8c2484281d35e089a25795b964270ee996a69e0
Nikita Koptelov <nick@koptelov.me>
2025-03-29 23:37:33 +0100

    pkg/errorbag: first implementation

---
+++ pkg/errorbag/errorbag.go
@@ -0,0 +1,109 @@
+// Package errorbag provides a way to associate
+// errors with arbitrary keys and values.
+//
+// Bags are used to add context to errors - as an example, you can use them to pass
+// HTTP/gRPC codes, error reasons, etc.
+//
+// Bags implement the error interface and can be
+// used with the errors, github.com/pkg/errors and other packages.
+package errorbag
+
+import (
+	"errors"
+)
+
+// Bag is a key-value pair with an error
+// associated with it.
+type Bag[K comparable, T any] interface {
+	error
+	Key() K
+	Value() T
+	Unwrap() error
+}
+
+// With creates a new bag with the given cause, key, and value.
+// You can wrap the same error chain many times to add many K/V pairs.
+//
+// Nil cause returns a nil Bag.
+//
+// The 'context' key rules apply; it's better to create your own key type and use it instead of a string to protect against collisions.
+func With[K comparable, T any](cause error, key K, value T) Bag[K, T] {
+	if cause == nil {
+		return nil
+	}
+	return container[K, T]{
+		cause: cause,
+		key:   key,
+		value: value,
+	}
+}
+
+// Get returns the value associated with the given key; recursing into the error chain to find it.
+//
+// It follows the errors.Unwrap() semantics; key-value pairs behind the errors.Join are unsupported.
+func Get[K comparable, T any](err error, key K) (*T, bool) {
+	for err != nil {
+		if bag, ok := err.(Bag[K, T]); ok {
+			if bag.Key() == key {
+				ret := bag.Value()
+				return &ret, true
+			}
+		}
+		err = errors.Unwrap(err)
+	}
+	return nil, false
+}
+
+// GetAll returns all the values associated with the given key; recursing into the error chain to find them.
+//
+// It follows the errors.Unwrap() semantics; key-value pairs behind the errors.Join are unsupported.
+func GetAll[K comparable, T any](err error, key K) ([]T, bool) {
+	var ret []T
+	for err != nil {
+		if bag, ok := err.(Bag[K, T]); ok {
+			if bag.Key() == key {
+				ret = append(ret, bag.Value())
+			}
+		}
+		err = errors.Unwrap(err)
+	}
+	return ret, len(ret) > 0
+}
+
+// ListPairs returns all the key-value pairs associated with the given error; recursing into the error chain to find them.
+//
+// Listing the pairs is a potentially expensive operation, as it requires
+// traversing the entire error chain.
+func ListPairs(err error) []any {
+	ret := make([]any, 0, 3)
+	for err != nil {
+		if bag, ok := err.(Bag[any, any]); ok {
+			ret = append(ret, bag.Key(), bag.Value())
+		}
+		err = errors.Unwrap(err)
+	}
+	return ret
+}
+
+type container[K comparable, T any] struct {
+	cause error
+
+	key   K
+	value T
+}
+
+func (c container[K, T]) Error() string {
+	return c.cause.Error()
+}
+
+func (c container[K, T]) Key() K {
+	return c.key
+}
+
+func (c container[K, T]) Value() T {
+	return c.value
+}
+
+func (c container[K, T]) Unwrap() error {
+	return c.cause
+}
