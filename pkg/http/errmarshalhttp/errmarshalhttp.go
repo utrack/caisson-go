1f3eb26703d0a5361629d3f89138eadcd7136269
Nikita Koptelov <nick@koptelov.me>
2025-04-19 18:27:12 +0200

    errmarshal: put error details as attributes

--- pkg/http/errmarshalhttp/errmarshalhttp.go
+++ pkg/http/errmarshalhttp/errmarshalhttp.go
@@ -9,9 +9,16 @@ import (
 	"github.com/longkai/rfc7807"
 	"github.com/utrack/caisson-go/errors"
 	"github.com/utrack/caisson-go/pkg/errorbag"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func Marshal(rspErr error, w http.ResponseWriter, r *http.Request) {
+
+	span := trace.SpanFromContext(r.Context())
+	if span != nil {
+		span.RecordError(rspErr)
+	}
 
 	code := errors.Code(rspErr)
 
@@ -33,12 +40,20 @@ func Marshal(rspErr error, w http.ResponseWriter, r *http.Request) {
 	pairs := errorbag.ListPairs(rspErr)
 	rsp.Extensions = pairs
 
+	if span != nil {
+		for k, v := range pairs {
+			// TODO: convert to attribute.Value
+			span.SetAttributes(attribute.String(k, fmt.Sprintf("%v", v)))
+		}
+	}
+
 	buf, err := json.Marshal(rsp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		slog.ErrorContext(r.Context(), "failed to marshal error", slog.String("err", err.Error()), slog.String("stack", fmt.Sprintf("%+v", err)))
 		return
 	}
+
 
 	w.Write(buf)
 }
